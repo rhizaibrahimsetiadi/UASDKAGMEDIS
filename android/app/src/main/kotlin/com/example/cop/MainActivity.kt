dd9d518e3437fb85df5146a719bffc394d8a08c0
rhizaibrahimsetiadi <rhizasetiadi@gmail.com>
2024-02-06 15:54:16 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/cop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:cop/view/login/login_view.dart';
+import 'package:cop/view/main_tab/main_tab_view.dart';
+import 'package:flutter/material.dart';
+
+import 'common/colo_extension.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MEDIS',
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(primaryColor: TColor.primaryColor1, fontFamily: "Poppins"),
+      home: const LoginView(),
+    );
+  }
+}
